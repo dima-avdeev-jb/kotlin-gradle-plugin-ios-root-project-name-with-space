2fb649ed55b4c79941eda600a375e0501e08eaa5
dima.avdeev <dima.avdeev@jetbrains.com>
2023-05-04 14:58:40 +0400

    remove redundant line with cocoapods resources

--- module1/build.gradle.kts
+++ module1/build.gradle.kts
@@ -17,7 +17,6 @@ kotlin {
             baseName = "shared"
             isStatic = true
         }
-        extraSpecAttributes["resources"] = "['src/commonMain/resources/**', 'src/iosMain/resources/**']"
     }
 
     sourceSets {
